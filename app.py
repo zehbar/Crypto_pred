a563a366a6a034af1377ff824709217296574df2
varad <varadsoham555@gmail.com>
2022-03-17 16:01:16 +0530

    Sample btc graph fix

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import plotly.express as px
 from coinpaprika import client as Coinpaprika
 
 app = Dash(__name__)
+server = app.server
 
 client = Coinpaprika.Client()
 df = client.historical("btc-bitcoin", start="2021-07-06T00:00:00Z")
@@ -10,12 +11,11 @@ df = client.historical("btc-bitcoin", start="2021-07-06T00:00:00Z")
 #print(df)
 fig = px.line(df, x='timestamp', y="price")
 app.layout = html.Div([
-    html.H4('GOOGLE stock prices'),
+    html.H4('BTC price line graph'),
     dcc.Graph(id="graph", figure=fig),
 ])
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
 
-    
-
-
-app.run_server(debug=True)
+# app.run_server(debug=True)
