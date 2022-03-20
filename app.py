4c0dac03e92c85448d2056dad284ae49acc86dbb
varad <varadsoham555@gmail.com>
2022-03-20 17:40:32 +0530

    added previous month prediction

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from dash import Dash, dcc, html, Input, Output
-import os
 import plotly.express as px
-from coinpaprika import client as Coinpaprika
+import requests
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -9,11 +8,19 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-client = Coinpaprika.Client()
-df = client.historical("btc-bitcoin", start="2021-07-06T00:00:00Z")
+url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin/ohlcv/historical?start=2021-07-06&end=2022-03-01"
+res = requests.get(url)
+obj=res.json()
+df = []
+for i in range(len(obj)):
+    newdict ={
+        "Price" : obj[i]["close"],
+        "Date" : obj[i]["time_close"]
+    }
+    df.append(newdict)
 #df = px.data.stocks()
 #print(df)
-fig = px.line(df, x='timestamp', y="price")
+fig = px.line(df, x='Date', y="Price")
 app.layout = html.Div([
     html.H4('BTC price line graph'),
     dcc.Graph(id="graph", figure=fig),
